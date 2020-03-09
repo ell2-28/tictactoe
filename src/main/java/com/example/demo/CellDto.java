c43d77037a3f0c555b23947fceaa7b4e56bc4810
ell2-228 <qwerty4383302>
2020-03-09 20:07:23 +0300

    Добавил в мавен lombok Добавил 2 DTO - для и мени и для ячейки Переделал логику обоих методов - теперь они работают через DTO

---
+++ src/main/java/com/example/demo/CellDto.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class CellDto {
+    String name;
+    Integer x;
+    Integer y;
+
+    public CellDto() {
+    }
+}
---
+++ src/main/java/com/example/demo/NameDto.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class NameDto {
+    String name;
+
+    public NameDto() {
+    }
+}
