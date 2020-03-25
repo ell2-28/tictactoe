b44b4615b73d4749daaf27f436d08cab1000d279
Eugen <ieugenor@gmail.com>
2020-03-26 00:32:28 +0300

    добавил комменты

--- src/main/java/Model/CheckWin.java
+++ src/main/java/Model/CheckWin.java
@@ -1,9 +1,9 @@
-package Model;
+package Model; // неправильное именование пакета
 import com.example.controller.Controller;
 
 public class CheckWin {
-    public boolean checkWin(char ch) {
-
+    public boolean checkWin(char ch) { // нужно завести енамку пох крестики и нолики
+        // нельзя ссылаться на статические поля контроллера
         if (Controller.field[0][0] == ch && Controller.field[0][1] == ch && Controller.field[0][2] == ch)
             return true;
         if (Controller.field[1][0] == ch && Controller.field[1][1] == ch && Controller.field[1][2] == ch)
